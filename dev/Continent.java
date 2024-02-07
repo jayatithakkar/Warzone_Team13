f3b6929dffe54c48cc38b7bc527c2a971caeb2ec
sushant-sinha <sinhasushant04@gmail.com>
2024-02-06 22:28:43 -0400

    added Continent.java

---
+++ dev/Continent.java
@@ -0,0 +1,34 @@
+public class Continent {
+    
+    String d_continentID;
+    int d_continentValue;
+    String d_continentColor;
+    
+    public void setContinentID(String p_continentID) {
+        // Add stub code for method
+        d_continentID = p_continentID;
+    }
+    
+    public String getContinentID() {
+        return d_continentID;
+    }
+    
+    public void setContinentValue(int p_continentValue) {
+        // Add stub code for method
+        d_continentValue = p_continentValue;
+    }
+    
+    public getContinentValue() {
+        return d_continentValue;
+    }
+    
+    public void setContinentColor(String p_continentColor) {
+        // Add stub code for method
+        d_continentColor = p_continentColor;
+    }
+    
+    public String getContinentColor() {
+        return d_continentColor;
+    }
+    
+}
