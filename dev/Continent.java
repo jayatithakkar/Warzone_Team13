9af87af5193334c6e253c5798e368e63292eec93
sushant-sinha <sinhasushant04@gmail.com>
2024-02-07 22:10:47 -0400

    added continentName and neighborCountries

--- dev/Continent.java
+++ dev/Continent.java
@@ -1,34 +1,36 @@
 public class Continent {
-    
+
     String d_continentID;
     int d_continentValue;
     String d_continentColor;
-    
+    String d_continentName;
+
     public void setContinentID(String p_continentID) {
-        // Add stub code for method
         d_continentID = p_continentID;
     }
-    
+
     public String getContinentID() {
         return d_continentID;
     }
-    
+
     public void setContinentValue(int p_continentValue) {
-        // Add stub code for method
         d_continentValue = p_continentValue;
     }
-    
+
     public getContinentValue() {
         return d_continentValue;
     }
-    
+
     public void setContinentColor(String p_continentColor) {
-        // Add stub code for method
         d_continentColor = p_continentColor;
     }
-    
+
     public String getContinentColor() {
         return d_continentColor;
     }
-    
+
+    public void setContinentName(String p_continentName) {
+        d_continentName = p_continentName;
+    }
+
 }
