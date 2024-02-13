b19acd2ce21c4a71f8e34344722ec8f242e54ab5
Rikin <64945337+rikinchauhan01@users.noreply.github.com>
2024-02-12 21:21:22 -0500

    Update PathResolverUtil.java
    Changes applied

--- src/main/java/com/APP/Project/UserCoreLogic/utilities/PathResolverUtil.java
+++ src/main/java/com/APP/Project/UserCoreLogic/utilities/PathResolverUtil.java
@@ -10,12 +10,9 @@ public class PathResolverUtil {
      private String d_user_data_directory = "";
 
      private PathResolverUtil() {
-          Path relative = Paths.get("file.txt");
-          System.out.println("Relative path: " + relative);
           Path absolute = relative.toAbsolutePath();
-          System.out.println("Absolute path: " + absolute);
           d_user_data_directory_path = Paths.get(System.getProperty("user.home"), "Downloads", d_user_data_directory);
-          System.out.println("NEW ------------------ " + d_user_data_directory_path);
+          
      }
 
      public static PathResolverUtil getInstance() {
@@ -30,8 +27,6 @@ public class PathResolverUtil {
      }
 
      public static String resolveFilePath(String p_filePath) {
-          System.out.println("------------------ Resolved "
-                    + Paths.get(getUserDataDirectoryPath().toString(), p_filePath).toString());
           return Paths.get(getUserDataDirectoryPath().toString(), p_filePath).toString();
      }
 }
