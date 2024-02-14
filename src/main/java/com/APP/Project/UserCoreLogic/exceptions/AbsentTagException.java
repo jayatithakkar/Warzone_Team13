42bfcf253e765e74c4f839ed42d52928d1d31263
rikinchauhan01 <rikinchauhan01@gmail.com>
2024-02-14 17:00:01 -0500

    JavaDoc comments added in all exception class

--- src/main/java/com/APP/Project/UserCoreLogic/exceptions/AbsentTagException.java
+++ src/main/java/com/APP/Project/UserCoreLogic/exceptions/AbsentTagException.java
@@ -1,10 +1,28 @@
 package com.APP.Project.UserCoreLogic.exceptions;
 
-public class AbsentTagException extends VirtualMachineException {
+/**
+ * This exception indicates that a required tag is absent in .map file.
+ * It extends the UserCoreLogicException class.
+ *
+ * @author Rikin Dipakkumar Chauhan
+ */
+public class AbsentTagException extends UserCoreLogicException {
+
+     /**
+     * Constructs an AbsentTagException with the specified detail message.
+     *
+     * @param p_exceptionMessage detailed Exception message
+     */
      public AbsentTagException(String p_exceptionMessage) {
           super(p_exceptionMessage);
      }
 
+     /**
+     * Constructs an AbsentTagException with the specified detail message and cause.
+     *
+     * @param p_exceptionMessage detailed Exception message
+     * @param p_exceptionCause detailed cause that causes the current exception
+     */
      public AbsentTagException(String p_exceptionMessage, Throwable p_exceptionCause) {
           super(p_exceptionMessage, p_exceptionCause);
      }
