ecc5969a17fad8252485bcc3573fd0f11762cc9e
bhooomyy <bhattbhoomi0111@gmail.com>
2024-02-15 21:59:15 -0500

    Repository files added
    repo for continent, country and player added

---
+++ src/main/java/com/APP/Project/UserCoreLogic/Container/ContinentContainer.java
@@ -0,0 +1,33 @@
+package com.APP.Project.UserCoreLogic.containers;
+
+import com.APP.Project.UserCoreLogic.GameEntities.Continent;
+import com.APP.Project.UserCoreLogic.exceptions.EntityNotFoundException;
+import com.APP.Project.UserCoreLogic.map_features.MapFeatureEngine;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ContinentContainer {
+    public List<Continent> searchByContinentName(String p_continentName) {
+        return MapFeatureEngine.getInstance().getContinentList().stream()
+                .filter(p_continent -> p_continent.getContinentName().equals(p_continentName))
+                .collect(Collectors.toList());
+    }
+
+    public Continent findFirstByContinentName(String p_continentName) throws EntityNotFoundException {
+        List<Continent> l_continentList = this.searchByContinentName(p_continentName);
+        if (l_continentList.size() > 0)
+            return l_continentList.get(0);
+        throw new EntityNotFoundException(String.format("'%s' continent not found", p_continentName));
+    }
+
+    public Continent findByContinentId(Integer p_continentId) throws EntityNotFoundException {
+        List<Continent> l_continentList = MapFeatureEngine.getInstance().getContinentList().stream()
+                .filter(p_continent -> p_continent.getUniqueContinentId().equals(p_continentId))
+                .collect(Collectors.toList());
+        if (!l_continentList.isEmpty()) {
+            return l_continentList.get(0);
+        }
+        throw new EntityNotFoundException(String.format("Continent with %s id not found!", p_continentId));
+    }
+}
