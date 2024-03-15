38c35de192f83620bc075736aee0acc892ba3a82
rikinchauhan01 <rikinchauhan01@gmail.com>
2024-03-15 17:24:52 -0400

    Added MapEditor phase, where map editing oerations can be performed

---
+++ src/main/java/com/APP/Project/UserCoreLogic/phases/MapEditor.java
@@ -0,0 +1,122 @@
+package com.APP.Project.UserCoreLogic.phases;
+
+import com.APP.Project.UserCoreLogic.GameEngine;
+import com.APP.Project.UserCoreLogic.exceptions.UserCoreLogicException;
+import com.APP.Project.UserCoreLogic.map_features.adapters.LoadMapAdapter;
+import com.APP.Project.UserCoreLogic.map_features.adapters.ShowMapAdapter;
+
+import java.util.List;
+
+import org.apache.maven.plugin.lifecycle.Phase;
+
+/**
+ * This abstract class represents the phase where map editing operations can be performed.
+ * It extends the Phase class and provides implementations for certain methods specific to map editing.
+ *
+ * @author Rikin Dipakkumar Chauhan
+ * @version 1.0
+ */
+public abstract class MapEditor extends Phase {
+    
+    /**
+     * Constructs a new MapEditor object with the specified GameEngine.
+     *
+     * @param p_gameEngine the GameEngine instance associated with this MapEditor
+     */
+    MapEditor(GameEngine p_gameEngine) {
+        super(p_gameEngine);
+    }
+
+    /**
+     * Loads a map using the specified list of arguments.
+     * It sets the game phase to PlaySetup after loading the map.
+     *
+     * @param p_arguments a list of arguments needed to load the map
+     * @return a string indicating the result of the load operation
+     * @throws UserCoreLogicException if an error occurs during the map loading process
+     */
+    @Override
+    public String loadMap(List<String> p_arguments) throws UserCoreLogicException {
+        LoadMapAdapter l_loadMapService = new LoadMapAdapter();
+        String l_returnValue = l_loadMapService.execute(p_arguments);
+        d_gameEngine.setGamePhase(new PlaySetup(d_gameEngine));
+        return l_returnValue;
+    }
+
+    /**
+     * Sets players for the game with the specified service type and arguments.
+     *
+     * @param p_serviceType the type of service for setting players
+     * @param p_arguments   the arguments needed for setting players
+     * @return a message indicating the invalidity of the command
+     */    
+    @Override
+    public String setPlayers(String p_serviceType, List<String> p_arguments) throws UserCoreLogicException {
+        return invalidCommand();
+    }
+
+    /**
+     * Assigns countries to players using the specified arguments.
+     *
+     * @param p_arguments the arguments needed for assigning countries
+     * @return a message indicating the invalidity of the command
+     */
+    @Override
+    public String assignCountries(List<String> p_arguments) throws UserCoreLogicException {
+        return invalidCommand();
+    }
+
+     /**
+     * Reinforces the game.
+     *
+     * @throws UserCoreLogicException if an error occurs during reinforcement
+     */
+    @Override
+    public void reinforce() throws UserCoreLogicException {
+        invalidCommand();
+    }
+
+    /**
+     * Issues orders for the game.
+     *
+     * @throws UserCoreLogicException if an error occurs during issuing orders
+     */
+    @Override
+    public void issueOrder() throws UserCoreLogicException {
+        invalidCommand();
+    }
+
+    /**
+     * Fortifies the game.
+     *
+     * @throws UserCoreLogicException if an error occurs during fortification
+     */
+    @Override
+    public void fortify() throws UserCoreLogicException {
+        invalidCommand();
+    }
+
+    /**
+     * Ends the game using the specified arguments.
+     *
+     * @param p_arguments the arguments needed for ending the game
+     * @throws UserCoreLogicException if an error occurs during the end game operation
+     */
+    @Override
+    public void endGame(List<String> p_arguments) throws UserCoreLogicException {
+        invalidCommand();
+    }
+
+    /**
+     * Shows the map using the specified arguments.
+     *
+     * @param p_arguments the arguments needed for showing the map
+     * @return the map as a string
+     * @throws UserCoreLogicException if an error occurs during the show map operation
+     */
+    @Override
+    public String showMap(List<String> p_arguments) throws UserCoreLogicException {
+        ShowMapAdapter l_showMapService = new ShowMapAdapter();
+        return l_showMapService.execute(p_arguments);
+    }
+}
