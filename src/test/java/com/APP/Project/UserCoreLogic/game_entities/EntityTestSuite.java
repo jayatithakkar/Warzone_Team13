a453d30ecfdc73b5022fda0c8ca523ecbe9f380f
sushant-sinha <sinhasushant04@gmail.com>
2024-03-16 15:34:53 -0400

    added entity test suite

---
+++ src/test/java/com/APP/Project/UserCoreLogic/game_entities/EntityTestSuite.java
@@ -0,0 +1,24 @@
+package com.APP.Project.UserCoreLogic.game_entities;
+
+import com.APP.Project.UserCoreLogic.game_entities.orders.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * This is a test suite for the test cases of different concrete classes of various game_entities.
+ *
+ * @author Sushant Sinha
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        AdvanceOrderTest.class,
+        AirliftOrderTest.class,
+        BlockadeOrderTest.class,
+        BombOrderTest.class,
+        DeployOrderTest.class,
+        PlayerTest.class,
+        NegotiateOrderTest.class
+})
+public class EntityTestSuite {
+    
+}
