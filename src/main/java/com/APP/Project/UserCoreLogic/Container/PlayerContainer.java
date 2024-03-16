7bd5b9372ee1487fe091b40649e6a1d7f8efb1cf
bhooomyy <bhattbhoomi0111@gmail.com>
2024-03-15 20:35:26 -0400

    Update PlayerContainer.java
    PlayerContainer.java updated

--- src/main/java/com/APP/Project/UserCoreLogic/Container/PlayerContainer.java
+++ src/main/java/com/APP/Project/UserCoreLogic/Container/PlayerContainer.java
@@ -2,41 +2,53 @@ package com.APP.Project.UserCoreLogic.Container;
 
 import com.APP.Project.UserCoreLogic.game_entities.Player;
 import com.APP.Project.UserCoreLogic.exceptions.EntityNotFoundException;
-import com.APP.Project.UserCoreLogic.gamePlay.GameEngine;
+import com.APP.Project.UserCoreLogic.gamePlay.GamePlayEngine;
 
-import java.util.*;
+import java.util.List;
 import java.util.stream.Collectors;
 
-/*
- * This class is responsible for searching player from game engine.
+/**
+ * This class finds the <code>Player</code> entity from the runtime engine.
+ *
  * @author Bhoomiben Bhatt
+ * @version 1.0
  */
 public class PlayerContainer {
-    /*
-     * finds player by it's name
+    /**
+     * Finds and returns the first player matching the specified name.
+     * This method searches through the list of players and returns the first one
+     * whose name exactly matches
+     * the provided name. If no such player is found, an EntityNotFoundException is
+     * thrown, indicating
+     * the absence of a player with the specified name.
+     *
+     * @param playerName The name of the player to search for. This parameter should
+     *                   not be null.
+     * @return The Player object matching the specified name.
+     * @throws EntityNotFoundException If no player with the given name can be
+     *                                 found.
      */
-    public Player searchByPlayerName(String p_playerName) throws EntityNotFoundException {
-        /*
-         * p_playerName is value of the name of the player.
-         * returns the value of first matched player
-         * if searching player not found then it will thrown a searching entity not
-         * found exception.
-         */
-        List<Player> l_filteredPlayerList = GameEngine.getInstance().getPlayerList().stream()
+    public Player findByPlayerName(String p_playerName) throws EntityNotFoundException {
+        List<Player> l_filteredPlayerList = GamePlayEngine.getInstance().getPlayerList().stream()
                 .filter(p_player -> p_player.getName().equals(p_playerName)).collect(Collectors.toList());
         if (l_filteredPlayerList.size() > 0)
             return l_filteredPlayerList.get(0);
         throw new EntityNotFoundException(String.format("'%s' player not found", p_playerName));
     }
 
-    /*
-     * this method is responsible for searching if player name already exists or
-     * not.
-     * p_playerName is name of searching player
-     * returns false if player isn't exists already. otherwise true.
+    /**
+     * Checks if a player with the specified name exists in the player list.
+     * This method searches through the list of players to see if at least one
+     * player has a name
+     * that exactly matches the provided name.
+     *
+     * @param playerName The name of the player to search for. This parameter should
+     *                   not be null.
+     * @return true if at least one player with the specified name exists; false
+     *         otherwise.
      */
     public boolean existByPlayerName(String p_playerName) {
-        return GameEngine.getInstance().getPlayerList().stream()
+        return GamePlayEngine.getInstance().getPlayerList().stream()
                 .filter(p_player -> p_player.getName().equals(p_playerName)).count() == 1;
     }
 }
