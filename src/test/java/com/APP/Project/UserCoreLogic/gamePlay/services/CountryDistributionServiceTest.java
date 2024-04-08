4cf345da1970ca080ae324eda9403478b4f6e202
Rupal27 <rupalkapoor70@yahoo.co.in>
2024-04-08 17:04:07 -0400

    adding changes for game play

--- src/test/java/com/APP/Project/UserCoreLogic/gamePlay/services/CountryDistributionServiceTest.java
+++ src/test/java/com/APP/Project/UserCoreLogic/gamePlay/services/CountryDistributionServiceTest.java
@@ -1,13 +1,13 @@
-package  com.APP.Project.UserCoreLogic.gamePlay.services;
+package com.APP.Project.UserCoreLogic.gamePlay.services;
 
-import  com.APP.Project.Main;
-import  com.APP.Project.UserCoreLogic.UserCoreLogic;
+import com.APP.Project.Main;
+import com.APP.Project.UserCoreLogic.UserCoreLogic;
+import com.APP.Project.UserCoreLogic.constants.enums.StrategyType;
+import com.APP.Project.UserCoreLogic.exceptions.InvalidInputException;
+import com.APP.Project.UserCoreLogic.exceptions.UserCoreLogicException;
+import com.APP.Project.UserCoreLogic.gamePlay.GameEngine;
+import com.APP.Project.UserCoreLogic.map_features.adapters.EditMapService;
 import com.APP.Project.UserCoreLogic.game_entities.Player;
-import com.APP.Project.UserCoreLogic.gamePlay.GamePlayEngine;
-import  com.APP.Project.UserCoreLogic.exceptions.InvalidInputException;
-import  com.APP.Project.UserCoreLogic.exceptions.UserCoreLogicException;
-import  com.APP.Project.UserCoreLogic.map_features.adapters.EditMapAdapter;
-
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -21,52 +21,53 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 /**
- * This class contains test cases for the CountryDistributionService class.
+ * This class tests various operations performed during distribution of countries among players.
  *
- * @author Jayati Thakkar
- * @author Rikin Dipakkumar Chauhan
+ * @author CHARIT
+ * @author Brijesh Lakkad
  */
 public class CountryDistributionServiceTest {
-    private static Main d_Main;
+    private static Main d_Application;
     private static URL d_TestFilePath;
     private CountryDistributionService d_distributeCountriesService;
-    private static GamePlayEngine d_GamePlayEngine;
+    private static GameEngine d_GamePlayEngine;
 
     /**
      * Runs before the test case class runs; Initializes different objects required to perform test.
+     *
+     * @throws IOException        If exception while coping the predefined files.
+     * @throws URISyntaxException If the path to parent directory of the files doesn't exist.
      */
     @BeforeClass
     public static void createPlayersList() throws IOException, URISyntaxException {
-        d_Main = new Main();
-        d_Main.handleApplicationStartup();
-        d_GamePlayEngine = GamePlayEngine.getInstance();
+        d_Application = new Main();
+        d_Application.handleApplicationStartup();
 
-        d_Main.restoreMapFiles();
+        // (Re)initialise the VM.
+        UserCoreLogic.getInstance().initialise();
+        d_GamePlayEngine = UserCoreLogic.getGameEngine().getGamePlayEngine();
+
+        d_Application.restoreMapFiles();
         d_TestFilePath = CountryDistributionServiceTest.class.getClassLoader().getResource("test_map_files/test_map.map");
     }
 
     /**
      * Setting up the required objects before performing test.
      *
-     * @throws UserCoreLogicException Exception generated during execution.
+     * @throws UserCoreLogicException        Exception generated during execution.
+     * @throws URISyntaxException If error while parsing the string representing the path.
      */
     @Before
-    public void before() throws UserCoreLogicException, URISyntaxException{
-        // (Re)initialise the UserCoreLogic.
-        UserCoreLogic.getInstance().initialise();
-
-
+    public void before() throws UserCoreLogicException, URISyntaxException {
+        d_GamePlayEngine.initialise();
         // Loads the map
-        EditMapAdapter l_editMapService = new EditMapAdapter();
+        EditMapService l_editMapService = new EditMapService();
         assertNotNull(d_TestFilePath);
         String l_url = new URI(d_TestFilePath.getPath()).getPath();
         l_editMapService.handleLoadMap(l_url);
 
-        Player l_player1 = new Player();
-        Player l_player2 = new Player();
-
-        l_player1.setName("User_1");
-        l_player2.setName("User_2");
+        Player l_player1 = new Player("User_1", StrategyType.HUMAN);
+        Player l_player2 = new Player("User_2", StrategyType.HUMAN);
 
         d_GamePlayEngine.addPlayer(l_player1);
         d_GamePlayEngine.addPlayer(l_player2);
--- src/test/java/com/APP/Project/UserCoreLogic/gamePlay/services/ReinforcementServiceTest.java
+++ src/test/java/com/APP/Project/UserCoreLogic/gamePlay/services/ReinforcementServiceTest.java
@@ -1,16 +1,17 @@
-package  com.APP.Project.UserCoreLogic.gamePlay.services;
+package com.APP.Project.UserCoreLogic.gamePlay.services;
 
-import com.APP.Project.UserCoreLogic.gamePlay.GamePlayEngine;
-import  com.APP.Project.UserCoreLogic.game_entities.Player;
-import  com.APP.Project.UserCoreLogic.exceptions.*;
-import  com.APP.Project.UserCoreLogic.map_features.MapFeatureEngine;
-import  com.APP.Project.UserCoreLogic.map_features.adapters.EditMapAdapter;
-
+import com.APP.Project.Main;
+import com.APP.Project.UserCoreLogic.UserCoreLogic;
+import com.APP.Project.UserCoreLogic.constants.enums.StrategyType;
+import com.APP.Project.UserCoreLogic.exceptions.*;
+import com.APP.Project.UserCoreLogic.gamePlay.GameEngine;
+import com.APP.Project.UserCoreLogic.map_features.MapEditorEngine;
+import com.APP.Project.UserCoreLogic.map_features.adapters.EditMapService;
+import com.APP.Project.UserCoreLogic.game_entities.Player;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -19,18 +20,18 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 /**
- * This file contains the ReinforcementServiceTest class which tests the functionality
- * of the ReinforcementService class.
- * @author Jayati Thakkar
- * @author Rikin Dipakkumar Chauhan
+ * This Class will test the reinforced army as in the reinforced army is set correctly or not.
+ *
+ * @author Rutwik
  */
 public class ReinforcementServiceTest {
-    private static MapFeatureEngine d_MapEditorEngine;
-    private static EditMapAdapter d_EditMapService;
+    private static Main d_Application = new Main();
+    private static MapEditorEngine d_MapEditorEngine;
+    private static EditMapService d_EditMapService;
     private static URL d_TestFile;
     private static CountryDistributionService d_DistributeCountriesService;
     private static ReinforcementService d_AssignReinforcementService;
-    private static GamePlayEngine d_GamePlayEngine;
+    private static GameEngine d_GamePlayEngine;
 
 
     /**
@@ -38,8 +39,10 @@ public class ReinforcementServiceTest {
      */
     @BeforeClass
     public static void beforeClass() {
-        d_GamePlayEngine = GamePlayEngine.getInstance();
-        d_MapEditorEngine = MapFeatureEngine.getInstance();
+        d_Application.handleApplicationStartup();
+        UserCoreLogic.getInstance().initialise();
+        d_GamePlayEngine = UserCoreLogic.getGameEngine().getGamePlayEngine();
+        d_MapEditorEngine = UserCoreLogic.getGameEngine().getMapEditorEngine();
         d_TestFile = ReinforcementServiceTest.class.getClassLoader().getResource("test_map_files/test_map.map");
     }
 
@@ -47,12 +50,12 @@ public class ReinforcementServiceTest {
     /**
      * Setting up the required Objects before test run.
      *
-     * @throws InvalidInputException Throws if provided argument and its value(s) are not valid.
-     * @throws AbsentTagException Throws if tag is absent in .map file.
-     * @throws InvalidMapException Throws if map file is invalid.
+     * @throws InvalidInputException     Throws if provided argument and its value(s) are not valid.
+     * @throws AbsentTagException        Throws if tag is absent in .map file.
+     * @throws InvalidMapException       Throws if map file is invalid.
      * @throws ResourceNotFoundException Throws if file not found.
-     * @throws EntityNotFoundException Throws if entity not found while searching.
-     * @throws URISyntaxException if the syntax is not right
+     * @throws EntityNotFoundException   Throws if entity not found while searching.
+     * @throws URISyntaxException        If error while parsing the string representing the path.
      */
     @Before
     public void before() throws InvalidInputException, AbsentTagException, InvalidMapException, ResourceNotFoundException, EntityNotFoundException, URISyntaxException {
@@ -61,13 +64,13 @@ public class ReinforcementServiceTest {
         d_MapEditorEngine.getCountryList();
         d_AssignReinforcementService = new ReinforcementService();
 
-        Player l_player1 = new Player();
-        Player l_player2 = new Player();
+        Player l_player1 = new Player("USER_1", StrategyType.HUMAN);
+        Player l_player2 = new Player("USER_1", StrategyType.HUMAN);
 
         d_GamePlayEngine.addPlayer(l_player1);
         d_GamePlayEngine.addPlayer(l_player2);
 
-        d_EditMapService = new EditMapAdapter();
+        d_EditMapService = new EditMapService();
         assertNotNull(d_TestFile);
         String l_url = new URI(d_TestFile.getPath()).getPath();
         d_EditMapService.handleLoadMap(l_url);
@@ -80,7 +83,7 @@ public class ReinforcementServiceTest {
      */
     @Test(expected = Test.None.class)
     public void testAssignCountry() {
-        for (Player l_player : GamePlayEngine.getInstance().getPlayerList()) {
+        for (Player l_player : d_GamePlayEngine.getPlayerList()) {
             assertNotNull(l_player.getAssignedCountries());
         }
     }
@@ -93,10 +96,10 @@ public class ReinforcementServiceTest {
     @Test
     public void testingCalculatedReinforcedArmyValue() throws EntityNotFoundException {
         d_AssignReinforcementService.execute();
-        int l_reinforcementArmies = GamePlayEngine.getInstance().getPlayerList().get(0).getReinforcementCount();
+        int l_reinforcementArmies = d_GamePlayEngine.getPlayerList().get(0).getReinforcementCount();
         assertEquals(9, l_reinforcementArmies);
 
-        int l_reinforcementArmies1 = GamePlayEngine.getInstance().getPlayerList().get(1).getReinforcementCount();
+        int l_reinforcementArmies1 = d_GamePlayEngine.getPlayerList().get(1).getReinforcementCount();
         assertEquals(13, l_reinforcementArmies1);
     }
 }
