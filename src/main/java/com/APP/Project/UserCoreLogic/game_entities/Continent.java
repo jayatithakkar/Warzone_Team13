00c224d51b2be2e4ac64293c566b5816a92e5df5
sushant-sinha <sinhasushant04@gmail.com>
2024-04-08 22:14:58 -0400

    updated remaining game_entitites

--- src/main/java/com/APP/Project/UserCoreLogic/game_entities/Continent.java
+++ src/main/java/com/APP/Project/UserCoreLogic/game_entities/Continent.java
@@ -1,30 +1,33 @@
 package com.APP.Project.UserCoreLogic.game_entities;
 
+import com.APP.Project.UserCoreLogic.constants.interfaces.JSONable;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.Objects;
 
 /**
- * This class provides the getter-setter methods for different operation on the Continent entity.
+ * This class provides different getter-setter methods to perform different operation on Continent entity.
  *
  * @author Sushant Sinha
  */
-public class Continent {
+public class Continent implements JSONable {
     /**
-     * Generated the id for different continents.
+     * Auto-generated ID of the continent.
      */
     private Integer d_continentId;
     private String d_continentName;
     private Integer d_continentControlValue;
-    private String d_continentColor;
     private ArrayList<Country> d_countryList;
 
     /**
-     * Needed for tracking the used IDs for the continent.
+     * Used to keep the track of unique IDs for the continent.
      */
     public static int d_SerialNumber = 0;
 
     /**
-     * Assigns the ID to the continent. Creates the countries list.
+     * Assigns Continent id to the continent and creates the member countries list.
      */
     public Continent() {
         this.d_continentId = ++d_SerialNumber;
@@ -32,7 +35,7 @@ public class Continent {
     }
 
     /**
-     * Gets the ID of the continent.
+     * Get the id of the continent which is the index where the continent is located.
      *
      * @return Value of the continent ID.
      */
@@ -41,7 +44,7 @@ public class Continent {
     }
 
     /**
-     * Sets the continent name.
+     * Sets the value of continent name.
      *
      * @param p_continentName Name of the continent.
      */
@@ -50,7 +53,7 @@ public class Continent {
     }
 
     /**
-     * Gets the continent name.
+     * Gets continent name.
      *
      * @return continent name.
      */
@@ -77,7 +80,7 @@ public class Continent {
     }
 
     /**
-     * Gets the list of countries present in this continent.
+     * Gets the countries of this continent.
      *
      * @return Value of the list of countries
      */
@@ -86,7 +89,7 @@ public class Continent {
     }
 
     /**
-     * Sets the list of countries present in this continent.
+     * Sets the country list for this continent.
      *
      * @param p_countryList Value of the list.
      */
@@ -95,16 +98,17 @@ public class Continent {
     }
 
     /**
-     * Adds a country to the list of countries present in this continent.
+     * Adds a single country to the list of countries belonging to this continent.
      *
      * @param p_country Value of the country to be added.
      */
     public void addCountry(Country p_country) {
+        // Set will not have any duplicate elements.
         d_countryList.add(p_country);
     }
 
     /**
-     * Removes the country from this continent.
+     * Removes country from the list.
      *
      * @param p_country Value of the country to be removed.
      */
@@ -114,14 +118,14 @@ public class Continent {
     }
 
     /**
-     * Resets the serial number to zero. Needed when the map engine is being reset.
+     * Resets the serial number to zero. Used when the map engine is being reset.
      */
     public static void resetSerialNumber() {
         d_SerialNumber = 0;
     }
 
     /**
-     * Checks if both objects are the same using continent ID of the object.
+     * Checks if both objects are the same using continent id of the object.
      *
      * @param l_p_o Value of the second element to be checked with.
      * @return True if the both are same.
@@ -142,5 +146,48 @@ public class Continent {
     @Override
     public int hashCode() {
         return Objects.hash(d_continentId);
+    }
+
+
+    /**
+     * Creates <code>JSONObject</code> using the runtime information stored in data members of this class.
+     *
+     * @return Created <code>JSONObject</code>.
+     */
+    @Override
+    public JSONObject toJSON() {
+        JSONObject l_continentJSON = new JSONObject();
+        l_continentJSON.put("name", d_continentName);
+        l_continentJSON.put("controlValue", d_continentControlValue);
+        JSONArray l_countryJSONList = new JSONArray();
+        for (Country l_country : getCountryList()) {
+            l_countryJSONList.put(l_country.toJSON());
+        }
+        l_continentJSON.put("countries", l_countryJSONList);
+        return l_continentJSON;
+    }
+
+    /**
+     * Creates an instance of this class and assigns the data members of the concrete class using the values inside
+     * <code>JSONObject</code>.
+     *
+     * @param p_jsonObject <code>JSONObject</code> holding the runtime information.
+     * @return Created instance of this class using the provided JSON data.
+     */
+    public static Continent fromJSON(JSONObject p_jsonObject) {
+        Continent l_continent = new Continent();
+        l_continent.setContinentName(p_jsonObject.getString("name"));
+        l_continent.setContinentControlValue(p_jsonObject.getInt("controlValue"));
+
+        // Create countries.
+        JSONArray l_countries = p_jsonObject.getJSONArray("countries");
+        for (int l_countryIndex = 0; l_countryIndex < l_countries.length(); l_countryIndex++) {
+            JSONObject l_countryJSON = l_countries.getJSONObject(l_countryIndex);
+            // Create new country instance.
+            Country l_countryObject = Country.fromJSON(l_countryJSON);
+            l_continent.addCountry(l_countryObject);
+        }
+
+        return l_continent;
     }
 }
