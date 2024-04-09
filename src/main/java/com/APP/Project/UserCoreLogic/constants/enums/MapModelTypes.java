dae17a2ea06888857e5269bdd28fdda8f3f1bef6
bhooomyy <bhattbhoomi0111@gmail.com>
2024-04-09 10:07:14 -0400

    Territory added to MapModelTypes.java

--- src/main/java/com/APP/Project/UserCoreLogic/constants/enums/MapModelTypes.java
+++ src/main/java/com/APP/Project/UserCoreLogic/constants/enums/MapModelTypes.java
@@ -1,65 +1,51 @@
 package com.APP.Project.UserCoreLogic.constants.enums;
 
 /**
- * Enumerates the types of models used to represent different aspects of a map
- * in the data being read from a file.
- * This enumeration is used to distinguish between the different kinds of data
- * models that are applicable to map construction and manipulation,
- * specifically focusing on continents, countries, and their borders. It
- * provides a clear way to handle different sections or types of data
- * within a map file, facilitating parsing and organization of map-related
- * information.
+ * Indicates the type of model from which data is being read in the file.
  *
  * @author Bhoomiben Bhatt
  * @version 1.0
  */
 public enum MapModelTypes {
     /**
-     * Represents data pertaining to the continents in a map. This type is used to
-     * identify and process
-     * information related to the continents' definitions within the map data.
+     * If map file contains Map tag.
+     */
+    MAP("Map"),
+    /**
+     * If model data belongs to continents of map
      */
     CONTINENT("continents"),
     /**
-     * Represents data pertaining to the countries in a map. This type is used to
-     * identify and process
-     * information related to the countries' definitions, including their
-     * associations with continents.
+     * If model data belongs to counties
      */
     COUNTRY("countries"),
     /**
-     * Represents data pertaining to the borders between countries in a map. This
-     * type is used to identify
-     * and process information related to how countries are interconnected through
-     * their borders.
+     * If model data belongs to neighbours of the countries
      */
-    BORDER("borders");
+    BORDER("borders"),
+    /**
+     * If the model data belongs to territories.
+     */
+    TERRITORY("Territories");
 
     /**
-     * The string representation of the enum value, designed for use with JSON
-     * serialization or similar contexts.
+     * Variable to set enum value.
      */
     public String d_jsonValue;
 
     /**
-     * Initializes the enum with a specific string value intended for JSON
-     * serialization or similar usage.
+     * Sets the string value of the enum.
      *
-     * @param p_jsonValue The string representation of the enum value.
+     * @param p_jsonValue Value of the enum.
      */
     private MapModelTypes(String p_jsonValue) {
         this.d_jsonValue = p_jsonValue;
     }
 
     /**
-     * Retrieves the string representation of the enum value.
-     * This method enables the retrieval of the enum's value in a format suitable
-     * for JSON serialization,
-     * aiding in the process of exporting or interpreting enum values within a
-     * data-exchange context.
+     * Gets the string value of the enum
      *
-     * @return The string representation of the enum, suitable for JSON
-     *         serialization.
+     * @return Value of the enum
      */
     public String getJsonValue() {
         return d_jsonValue;
