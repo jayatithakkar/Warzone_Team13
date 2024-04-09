4fd75326fc0ddfe52be9bc88916d95c8b27b5df0
bhooomyy <157864609+bhooomyy@users.noreply.github.com>
2024-04-09 10:57:06 -0400

    Update CardType.java

--- src/main/java/com/APP/Project/UserCoreLogic/constants/enums/CardType.java
+++ src/main/java/com/APP/Project/UserCoreLogic/constants/enums/CardType.java
@@ -6,21 +6,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Enumerates different types of cards used in the game, each representing a
- * specific action or effect.
- * This enumeration defines the types of cards that can be used within the game,
- * providing a way to differentiate
- * between various game actions such as airlift, blockade, bomb, and diplomacy.
- * It also includes an EMPTY type
- * for scenarios where no card action is applicable. The enum facilitates
- * managing game logic related to cards
- * by providing a typed list of all possible card actions, excluding the EMPTY
- * type, through the {@link #usableCardList()} method.
+ * This enum represents the type of card.
  *
- * @see Card An interface that these card types might implement or interact
- *      with.
- * @version 1.0
  * @author Bhoomiben Bhatt
+ * @version 1.0
+ * @see Card
  */
 public enum CardType {
     /**
@@ -45,31 +35,23 @@ public enum CardType {
     DIPLOMACY("diplomacy");
 
     /**
-     * The JSON-compatible string representation of the enum value.
+     * Variable to set enum value.
      */
     public String d_jsonValue;
 
     /**
-     * Initializes the enum value with a string representation suitable for JSON
-     * serialization.
+     * Sets the string value of the enum.
      *
-     * @param p_jsonValue The JSON-compatible string representation of the enum.
+     * @param p_jsonValue is Value of the enum.
      */
     private CardType(String p_jsonValue) {
         this.d_jsonValue = p_jsonValue;
     }
 
     /**
-     * Provides a list of card types that are usable in the game, excluding the
-     * EMPTY type.
-     * <p>
-     * This method returns a list of all card types that represent actionable cards
-     * within the game,
-     * allowing for easy identification and filtering of cards that can be used
-     * during gameplay.
-     * </p>
+     * Gets usable card list.
      *
-     * @return A list of {@link CardType} excluding {@link CardType#EMPTY}.
+     * @return Value of card list.
      */
     public static List<CardType> usableCardList() {
         List<CardType> l_cardTypeList = new ArrayList<>();
@@ -81,10 +63,9 @@ public enum CardType {
     }
 
     /**
-     * Retrieves the string representation of the enum value, suitable for JSON
-     * serialization.
+     * Gets the string value of the enum
      *
-     * @return The JSON-compatible string representation of the enum.
+     * @return Value of the enum
      */
     public String getJsonValue() {
         return d_jsonValue;
