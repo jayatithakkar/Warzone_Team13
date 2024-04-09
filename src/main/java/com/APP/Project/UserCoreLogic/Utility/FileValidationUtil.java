ad1baa035fd61cec7c746d685229a6f169b0bd00
bhooomyy <bhattbhoomi0111@gmail.com>
2024-04-09 09:14:51 -0400

    FileValidationUtil.java and FindFilePathUtil.java Updated to make code more readable

--- src/main/java/com/APP/Project/UserCoreLogic/Utility/FileValidationUtil.java
+++ src/main/java/com/APP/Project/UserCoreLogic/Utility/FileValidationUtil.java
@@ -1,5 +1,6 @@
 package com.APP.Project.UserCoreLogic.Utility;
 
+import com.APP.Project.UserCoreLogic.constants.enums.FileType;
 import com.APP.Project.UserCoreLogic.exceptions.InvalidInputException;
 import com.APP.Project.UserCoreLogic.exceptions.ResourceNotFoundException;
 
@@ -9,14 +10,9 @@ import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 
 /**
- * Provides utility methods for resolving file paths within user-defined data
- * and log directories.
- * This class implements a singleton pattern to ensure a single instance manages
- * path resolutions throughout the application. It automatically creates user
- * data and log directories if they do not exist, using paths based on the
- * user's home directory. This utility is designed to centralize the management
- * of file paths, facilitating easier access and manipulation of files within
- * these directories.
+ * This class facilitates common file operations(copying file,creating new file
+ * if does not exists, retrieving existing file etc..), offering a suite of
+ * services for managing file I/O tasks.
  *
  * @author Bhoomiben Bhatt
  * @version 1.0
@@ -24,24 +20,65 @@ import java.nio.file.StandardCopyOption;
 public class FileValidationUtil {
     private static final String MAP_FILE_EXTENSION = "map";
 
+    private static final String GAME_EXTENSION = "warzone";
+
     /**
-     * Retrieves the designated file extension for valid files.
+     * Gets the value of the valid available file extension.
      *
-     * @return A {@code String} representing the valid file extension.
+     * @return Value of the file extension.
      */
     public static String getFileExtension() {
         return MAP_FILE_EXTENSION;
     }
 
     /**
-     * Checks whether the given file name is valid or not.
+     * Validate whether the given map file name is valid or not and correspondingly
+     * retrieve it. Uses overloading method
      *
-     * @param p_filePath Value of the path to file.
+     * @param p_filePath Value of the path to game file.
      * @return Value of File object for the file given with path.
-     * @throws InvalidInputException     Throws if the file does not exist.
-     * @throws ResourceNotFoundException Throws if file can not be created.
+     * @throws InvalidInputException     Throws if the file does not exist or path
+     *                                   invalid.
+     * @throws ResourceNotFoundException Throws if file can not be created or
+     *                                   accessed.
      */
-    public static File retrieveFile(String p_filePath) throws ResourceNotFoundException, InvalidInputException {
+    public static File retrieveMapFile(String p_filePath) throws ResourceNotFoundException, InvalidInputException {
+        return retrieveFile(p_filePath, FileType.MAP);
+    }
+
+    /**
+     * Checks whether the given game file name is valid or not. Uses overloading
+     * method
+     * <code>FileUtil#retrieveFile(String, FileType)</code>.
+     *
+     * @param p_filePath Value of the path to to the game file being verified and
+     *                   retrieved.
+     * @return Value of File object for the file given with path.
+     * @throws InvalidInputException     Throws if the file does not exist,
+     *                                   indicating invalid path or filename.
+     * @throws ResourceNotFoundException Throws if file can not be created or
+     *                                   accessed, indicating potential issue with
+     *                                   file permission or the existence of the
+     *                                   file.
+     */
+    public static File retrieveGameFile(String p_filePath) throws ResourceNotFoundException, InvalidInputException {
+        return retrieveFile(p_filePath, FileType.GAME);
+    }
+
+    /**
+     * Retrieves the file using file type and its name.
+     *
+     * @param p_filePath Value of the path to the game file to be retrieved.
+     * @param p_fileType Type of the file.
+     * @return Value of File object for the file given with path, if available and
+     *         accessible.
+     * @throws InvalidInputException     Throws if the file does not exist,
+     *                                   indicating invalid path or filename.
+     * @throws ResourceNotFoundException Throws if file can not be created or
+     *                                   accessed.
+     */
+    public static File retrieveFile(String p_filePath, FileType p_fileType)
+            throws ResourceNotFoundException, InvalidInputException {
         File l_file = new File(p_filePath);
         String l_fileName = l_file.getName();
         try {
@@ -50,45 +87,60 @@ public class FileValidationUtil {
             throw new ResourceNotFoundException("Can not create a file due to file permission!");
         }
 
-        try {
-            if (checksIfFileHasRequiredExtension(l_fileName)) {
-                return l_file;
-            }
-        } catch (InvalidInputException p_invalidInputException) {
-            throw p_invalidInputException;
+        if (checksIfFileHasRequiredExtension(l_fileName, p_fileType)) {
+            return l_file;
         }
 
         throw new InvalidInputException("Invalid file!");
     }
 
     /**
-     * Validates whether the provided file name has the required extension.
+     * Determines if the specified file matches the required extension, utilizing
+     * <code>FileType</code>
+     * for identifying the file's category. This method checks the file's extension
+     * against the expected type
+     * defined by <code>FileType</code>.
      *
-     * @param p_fileName The name of the file as a {@code String}.
-     * @return {@code true} if the file has the required extension; otherwise {@code false}.
-     * @throws InvalidInputException if the file name is invalid or does not have an extension.
+     * @param p_fileName Name of the file being checked.
+     * @param p_fileType The expected type of the file, used to verify the file's
+     *                   extension.
+     * @return True the file's extension aligns with the required FileType;
+     *         otherwise false.
+     * @throws InvalidInputException Throws If the provided filename is deemed
+     *                               invalid.
      */
-    public static boolean checksIfFileHasRequiredExtension(String p_fileName) throws InvalidInputException {
+    public static boolean checksIfFileHasRequiredExtension(String p_fileName, FileType p_fileType)
+            throws InvalidInputException {
         int l_index = p_fileName.lastIndexOf('.');
         if (l_index > 0) {
             char l_prevChar = p_fileName.charAt(l_index - 1);
             if (l_prevChar != '.') {
                 String l_extension = p_fileName.substring(l_index + 1);
-                if (!l_extension.equalsIgnoreCase(FileValidationUtil.getFileExtension())) {
-                    throw new InvalidInputException("File doesn't exist!");
+                if (p_fileType == FileType.MAP &&
+                        l_extension.equalsIgnoreCase(FileValidationUtil.getFileExtension())) {
+                    return true;
+                } else if (p_fileType == FileType.GAME &&
+                        l_extension.equalsIgnoreCase(FileValidationUtil.getGameExtension())) {
+                    return true;
                 }
-                return true;
+                throw new InvalidInputException("File doesn't exist!");
             }
         }
         throw new InvalidInputException("File must have an extension!");
     }
 
     /**
-     * Creates a new file at the specified path if it does not already exist.
+     * Ensures the existence of a file at the specified path by creating it if it
+     * does not already exist.
+     * This method attempts to establish a new file based on the provided path and
+     * returns a File object.
      *
-     * @param p_filePath The file path as a {@code String}.
-     * @return A {@code File} object for the newly created file.
-     * @throws ResourceNotFoundException if the file cannot be created.
+     * @param p_filePath The path where the file is to be created
+     * @return A File object corresponding to the newly created or existing file at
+     *         the specified path.
+     * @throws ResourceNotFoundException If the file cannot be found or created,
+     *                                   likely due to issues with the path or file
+     *                                   system permissions.
      */
     public static File createFileIfNotExists(String p_filePath) throws ResourceNotFoundException {
         File l_file = new File(p_filePath);
@@ -101,11 +153,13 @@ public class FileValidationUtil {
     }
 
     /**
-     * Checks if the file exists or not.
+     * Verifies the existence of a file represented by the given file object.
      *
-     * @param p_fileObject Value of the file object.
-     * @return True if the file exists; otherwise throws an exception.
-     * @throws ResourceNotFoundException Throws if file not found.
+     * @param p_fileObject The file object to check for existence.
+     * @return True if the file exists, indicating a successful check.
+     * @throws ResourceNotFoundException If the file does not exist, indicating the
+     *                                   file object points to an invalid or
+     *                                   inaccessible file.
      */
     private static boolean checkIfFileExists(File p_fileObject) throws ResourceNotFoundException {
         if (!p_fileObject.exists()) {
@@ -115,10 +169,15 @@ public class FileValidationUtil {
     }
 
     /**
-     * Copies the file from source path to the destination. This method replaces the existing file at destination path.
+     * Copies a file from a source path to a specified destination path. If a file
+     * already exists at the destination path,
+     * it will be overwritten by the file from the source path. This method ensures
+     * that the exact content of the source file
+     * is replicated at the destination, making it suitable for file backup or
+     * duplication tasks.
      *
-     * @param p_source Source path to the file.
-     * @param p_dest   Destination path to the file.
+     * @param p_source The path of the file to be copied.
+     * @param p_dest   The path where the file should be copied to.
      */
     public static void copy(Path p_source, Path p_dest) {
         try {
@@ -129,5 +188,14 @@ public class FileValidationUtil {
         } catch (Exception l_ignored) {
             // Ignore the exception while copying.
         }
+    }
+
+    /**
+     * Gets the game extension.
+     *
+     * @return Value of the game extension.
+     */
+    public static String getGameExtension() {
+        return GAME_EXTENSION;
     }
 }
